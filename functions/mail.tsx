23c8a6ff1fa84b0e43ef9e39a4bd96716d8ffc7f
HoWeiHao <howeihao3105@gmail.com>
2024-01-20 19:02:02 +0800

    Implement email API

--- functions/mail.tsx
+++ functions/mail.tsx
@@ -1,8 +1,25 @@
+const mail = require("@mailersend");
+
+mail.setApiKey(process.env.MAILERSEND_API_KEY);
+
 exports.handler = async function (event, context) {
     const body = JSON.parse(event.body);
-    console.log('body', body)
+    const message = `
+    Name: ${body.name}rn
+    Email: ${body.email}rn
+    Message: ${body.message}
+    `;
+
+    await mail.send({
+        to:'howeihao3105@gmail.com',
+        from:'howeihao3105@gmail.com',
+        subject:  `New mail from ${body.name}`,
+        text: message,
+        html:message.replace(/rn/g, '<br>')
+    })
+
     return{
         statusCode: 200,
-        body: JSON.stringify({messages: 'Success!'})
+        body: JSON.stringify({success: true})
     }
 };
